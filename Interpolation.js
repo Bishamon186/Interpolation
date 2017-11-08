58636349a0d59f62a08484046b79f9600c54d764
Your NameBishamon186 <bishamon18186@gmail.com>
2017-11-09 01:17:46 +0300

    It is necessary to change the values

--- Interpolation.js
+++ Interpolation.js
@@ -2,11 +2,46 @@
 
 function check()
 {
-
+	document.getElementById('status').innerHTML = "";
+	
     var inp = document.getElementsByName("Rb");
-    var check;
+   
+    var check = 0;
     for (var i = 0; i < inp.length; i++) 
     {
+        if (inp[i].checked)
+        {     
+       	 check = inp[i].value;            
+        }
+        
+    }
+
+    if(check!=0)
+    {
+
+    	InterpolateLagrangePolynomial(check);	
+   	
+    }
+    else
+    {
+    	alert("Error: select points");
+    	document.getElementById('status').innerHTML = "Error: radio button";
+    }
+    
+}
+
+function checkOk(){
+	
+	document.getElementById("Ok");
+
+	 var number = document.getElementById("number").value;
+	 var number2;
+	 var result;
+	  var inp = document.getElementsByName("Rb");
+	  var check;
+
+    for (var i = 0; i < inp.length; i++) 
+    {		
         if (inp[i].checked)
         {     
        	 check = inp[i].value;            
@@ -17,15 +52,27 @@ function check()
         }
     }
 
-    if(check!=0)
-    {
-    	InterpolateLagrangePolynomial(check);
+    if (check==0){
+    	alert("Error: select points and interpolate function");
     }
-    else
-    {
-    	alert("Error: radio button ")
+    else {
+    	number2 = Number(number);
+    	 
+    	switch (check)
+	{
+		case '1':
+
+		xValuesOriginalFunc = [-1.5,-0.75,0,0.75,1.5]; 
+		yValuesOriginalFunc = [-14.101,-0.9315,0,0.9316,14.1014];
+		result = Polynomial(number2,xValuesOriginalFunc,5);	
+		alert("F("+number2+")=" +result);
+    	document.getElementById("F(x)").innerHTML = "F("+number2+")=" +result;           
+		break;
+		default:
+		alert("Error");
+		break;
+	}	
     }
-    
 }
 
    
@@ -34,26 +81,22 @@ function InterpolateLagrangePolynomial(NumbXValue)
 	var xValuesOriginalFunc;
 	var yValuesOriginalFunc;
 	var yValuesInterpolatedFunction=[];
-
+	var size
 	switch (NumbXValue)
 	{
 		case '1':
-
+		size = 5;
 		xValuesOriginalFunc = [-1.5,-0.75,0,0.75,1.5]; 
 		yValuesOriginalFunc = [-14.101,-0.9315,0,0.9316,14.1014];
-			for (var i = 0; i < 5; i++) 
+			for (var i = 0; i < size; i++) 
 			{
 
-				yValuesInterpolatedFunction.push(Polynomial(xValuesOriginalFunc[i],xValuesOriginalFunc,5));	
+				yValuesInterpolatedFunction.push(Polynomial(xValuesOriginalFunc[i],xValuesOriginalFunc,size));	
 				
             }
-            
-            for (i=0; i<5;i++){
+                      
 
-            }
-            
-
-        DrawingGraphs(xValuesOriginalFunc, yValuesOriginalFunc,yValuesInterpolatedFunction);    
+        DrawingGraphs(xValuesOriginalFunc, yValuesOriginalFunc,yValuesInterpolatedFunction,size);    
 
 		break;
 		default:
@@ -86,25 +129,24 @@ function Polynomial (x, xValuesOriginalFunc, size)
 }
 
 function FunctionValueEvaluation(x)
-{
-	
+{	
 	return Math.tan(x);
 }
 
-function DrawingGraphs( xValue, yValueOriginalFunc, yValuesInterpolatedFunction)
+function DrawingGraphs( xValue, yValueOriginalFunc, yValuesInterpolatedFunction,size)
 {
 google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback ( function () {drawChart1 (xValue,yValueOriginalFunc);} );
-google.charts.setOnLoadCallback ( function () {drawChart2 (xValue,yValuesInterpolatedFunction);} );
+google.charts.setOnLoadCallback ( function () {drawChart1 (xValue,yValueOriginalFunc,size);} );
+google.charts.setOnLoadCallback ( function () {drawChart2 (xValue,yValuesInterpolatedFunction,size);} );
 }        
      
-function drawChart1( xValue, yValueOriginalFunc) {      
+function drawChart1( xValue, yValueOriginalFunc,size) {      
 
 var data = new google.visualization.DataTable();  
  data.addColumn('number', 'X');
  data.addColumn('number', 'OriginalFunc');
 
-  for (var i =0; i < 5; i++) {
+  for (var i =0; i < size; i++) {
 
   	data.addRows([[xValue[i],yValueOriginalFunc[i]]]); 
   	} 
@@ -125,13 +167,13 @@ var data = new google.visualization.DataTable();
       chart.draw(data, options);
   }
 
-  function drawChart2( xValue,  yValuesInterpolatedFunction) {    
+  function drawChart2( xValue,  yValuesInterpolatedFunction,size) {    
 
 var data = new google.visualization.DataTable();  
  data.addColumn('number', 'X');
  data.addColumn('number', 'InterpolatedFunc');
 
-  for (var i =0; i < 5; i++) {
+  for (var i =0; i < size; i++) {
 
   	data.addRows([[xValue[i],yValuesInterpolatedFunction[i]]]); 
   	} 
